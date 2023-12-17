eda8c316500c4867000d725edc4efa93069d2645
abaines1 <114964796+abaines1@users.noreply.github.com>
2023-12-17 09:55:12 -0500

    Migrated from SQLite to PostgreSQL using ElephantSQL server

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-'''
+"""
 Phase 3 Project 2
 
 Database:
@@ -6,14 +6,24 @@ Database:
 Movies with id, title, release_timestamp
 Users with username
 Watched with users_username, movie_id - Used as a bridge table between movies and users to get watched movies by a user.
-'''
+"""
 
-import sqlite3
+import os
+import psycopg2
 import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+#Changing INTEGER to SERIAL : guaranteed unique autoincrement
+#Change (?) to (%s)
+#Change connection.execute() to cursor.execute
+
 
 # Constant vars
 CREATE_MOVIES_TABLE = '''CREATE TABLE IF NOT EXISTS movies (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     title TEXT, 
     release_timestamp REAL
     );'''
@@ -26,22 +36,21 @@ CREATE_WATCHED_TABLE = '''CREATE TABLE IF NOT EXISTS watched (
     FOREIGN KEY(users_username) REFERENCES users(username),
     FOREIGN KEY(movies_id) REFERENCES movies(id));'''
 
-INSERT_MOVIE = "INSERT INTO movies(title, release_timestamp) VALUES (?,?);"
-INSERT_NEW_USER = "INSERT INTO users(username) VALUES (?);"
+INSERT_MOVIE = "INSERT INTO movies(title, release_timestamp) VALUES (%s,%s);"
+INSERT_NEW_USER = "INSERT INTO users(username) VALUES (%s);"
 SELECT_MOVIES = "SELECT * FROM movies;"
-SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
+SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > %s;"
 
 SELECT_WATCHED_MOVIES = '''SELECT movies.* FROM movies
     JOIN watched ON movies.id = watched.movies_id
     JOIN users ON watched.users_username = users.username
-    WHERE users.username = ?;'''
+    WHERE users.username = %s;'''
 
-INSERT_WATCHED_MOVE = "INSERT INTO watched(users_username, movies_id) VALUES (?,?);"
-CHECK_MOVIE_EXISTS = "SELECT * FROM movies WHERE id = ?;"
-CHECK_USER_EXISTS = "SELECT username FROM users WHERE username = ?;"
+INSERT_WATCHED_MOVE = "INSERT INTO watched(users_username, movies_id) VALUES (%s,%s);"
+CHECK_MOVIE_EXISTS = "SELECT * FROM movies WHERE id = %s;"
+CHECK_USER_EXISTS = "SELECT username FROM users WHERE username = %s;"
 
-connection = sqlite3.connect("movies.db")
-
+connection = psycopg2.connect(os.environ['DATABASE_URL'])
 
 def normalize_username(username):
     # put other norms here.
@@ -49,19 +58,22 @@ def normalize_username(username):
 
 def create_tables():
     with connection:
-        connection.execute(CREATE_MOVIES_TABLE)
-        connection.execute(CREATE_USERS_TABLE)
-        connection.execute(CREATE_WATCHED_TABLE)
+        cursor = connection.cursor()
+        cursor.execute(CREATE_MOVIES_TABLE)
+        cursor.execute(CREATE_USERS_TABLE)
+        cursor.execute(CREATE_WATCHED_TABLE)
 
 
 def add_movie(title, release_timestamp):
     with connection:
-        connection.execute(INSERT_MOVIE, (title, release_timestamp))
+        cursor = connection.cursor()
+        cursor.execute(INSERT_MOVIE, (title, release_timestamp))
 
 
 def add_user(username):
     with connection:
-        connection.execute(INSERT_NEW_USER, (normalize_username(username),))
+        cursor = connection.cursor()
+        cursor.execute(INSERT_NEW_USER, (normalize_username(username),))
 
 
 def view_movies(upcoming=False):
@@ -81,9 +93,10 @@ def view_watched_movies(username):
 
 
 def watch_movie(movie_id, username):
+    cursor = connection.cursor();
     if movie_exists(movie_id):
         with connection:
-            connection.execute(INSERT_WATCHED_MOVE, (normalize_username(username), movie_id))
+            cursor.execute(INSERT_WATCHED_MOVE, (normalize_username(username), movie_id))
         return True
     else:
         return False
